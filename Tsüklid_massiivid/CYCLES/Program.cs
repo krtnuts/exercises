5c28c2dd180053c5c145087ef399b2382b9b5d5d
Opilane TTHK <opilane@tthk.ee>
2026-01-09 14:02:44 +0200

    Ülesane CYCLES DOWHILE-2 koos vooskemiga

--- Tsüklid_massiivid/CYCLES/Program.cs
+++ Tsüklid_massiivid/CYCLES/Program.cs
@@ -31,61 +31,76 @@
             // -> do while tsükli tingimuseks kirjuta kontroll, mis kontrollib muutuja "sõnum" pikkust .Length abil, kui sõnum on pikem kui 140 tähte, siis tsükkel lõppeb
             // peale tsüklit kuva sõnum "see sõnum enam twitterisse ei mahuks, kirjuta vähem fam"
 
-            /* While ülesanded*/
+            string sõnum = "";
+            do
+            {
+                Console.WriteLine("Palun lisa järgmine sõnumi osa: ");
+                sõnum += Console.ReadLine();
+                Console.WriteLine(sõnum);
+            } while (sõnum.Length < 140);
+            Console.WriteLine("See sõnum enam twitterisse ei mahuks, kirjuta vähem fäm");
 
-            // 1. "niikaua kuni ohtu pole"
-            // tee muutuja "CO2tase" mille sees on komakohaga float 0.0f
-            // kirjuta while tsükkel
-            // -> while tsükli tingimuseks kirjuta kontroll, mis kontrollib kas muutujas "CO2tase" on arv väiksem kui 40.000
-            // tsükli tegevusena kuva sõnum "mis on co2 tase?"
-            // pane muutujasse "CO2tase" info kasutajalt
-            // tee tingimuslauseline kontroll, mis kontrollib, kui kasutaja sisestus on väiksem kui 40.000,
-            // siis kuva sõnum kasutajale teavitades, et otsest ohtu ei ole
-            // peale tsüklit kuva sõnum "evakueeri hoonest, süsihappegaasi tase on liiga kõrgel"
-            /* 
-             * NB programm peab niikaua käima kuni kasutaja sisestus on suurem või võrdne kui 40.000
-             */
 
-            // 2. "vibumees"
-            // tee muutuja "mehike" mille sees on täisarv 0
-            // tee muutuja "vastus" mille sees on tühi tekst
-            // kirjuta while tsükkel
-            // -> while tsükli tingimuseks kirjuta kontroll:
-            //      mis kontrollib kas muutujas "mehike" on täisarv 5 või vähem
-            //      või
-            //      muutujas "vastus" on tekst "jah"
-            // tsükli tegevuseks küsi kasutajalt "kas tahad mehikesse noole lasta?"
-            // tsükli tegevuseks pane muutujasse "vastus" väärtus kasutajalt käsurea pealt 
-            // tsükli tegevuseks kontrolli tingimuslausega kas vastus oli "jah"
-            // tsükli tegevuseks Kui tingimuslause vastus oli jah, siis:
-            // kuva kasutajale tekst "mitu noolt lased?"
-            // lisa muutujasse "mehike" kasutajalt saadud täisarv
-            // peale tsüklit kontrolli kas muutujas "mehike" olev arv on väiksem kui 5
-            // kui on väiksem kui 5 siis kuva sõnum "mehike elab :D"
-            // kõikidel muudel juhtudel kuva sõnum "tapsid mehikese ära :c"
 
-            /* For ülesanded*/
 
-            // 1. "prindi numbrid"
-            // tee muutuja "kuipalju" milles on täisarv 0 
-            // kuva kasutajale tekst millega küsid kasutajalt mitu numbrit ta tahab
-            // omista käsurealt saadud arv muutujasse "kuipalju"
-            // kirjuta for tsükkel, tsükli teise parameetrisse - kontrolli aseta i vastu muutuja "kuipalju"
-            // tsükli tegevusena kuva kasutajale välja i, aga liida sellele üks juurde et lugemine algaks arvust 1
-            // peale tsüklit kuva kasutajale sõnum "tsükkel lõppes"
 
-            // 2. "Ruut"
-            // tee muutuja "ruudukülg" milles on täisarv 0
-            // kuva kasutajale tekst millega küsid kasutajalt kui suurt ruutu ta tahab
-            // omista käsurealt saadud arv muutujasse "ruudukülg"
-            // kirjuta for tsükkel, tsükli tingimuse kontrolli pane i kontrollimisse muutuja "ruudukülg"
-            // tsükli sisse kirjuta sõne tüüpi muutuja, "seeRida", kuhu omistad tühja sõne.
-            // tsükli sees on omakorda teine for tsükkel, ka selle tingimuse kontrolli pane i kontrollimisse muutuja "ruudukülg"'
-            // nüüd sisemise tsükli sees, omista muutujale "seeRida" juurde liites sisse väärtus "HH" 
-            // kui sisemine tsükkel on lõpetanud, siis kuva kasutajale see rida välja
-            // kui ka esimene tsükkel on lõpetanud, siis kuva kasutajale tekst "tsüklid on lõpetanud"
 
-            int ruuduKülg = 0;
+                /* While ülesanded*/
+
+                // 1. "niikaua kuni ohtu pole"
+                // tee muutuja "CO2tase" mille sees on komakohaga float 0.0f
+                // kirjuta while tsükkel
+                // -> while tsükli tingimuseks kirjuta kontroll, mis kontrollib kas muutujas "CO2tase" on arv väiksem kui 40.000
+                // tsükli tegevusena kuva sõnum "mis on co2 tase?"
+                // pane muutujasse "CO2tase" info kasutajalt
+                // tee tingimuslauseline kontroll, mis kontrollib, kui kasutaja sisestus on väiksem kui 40.000,
+                // siis kuva sõnum kasutajale teavitades, et otsest ohtu ei ole
+                // peale tsüklit kuva sõnum "evakueeri hoonest, süsihappegaasi tase on liiga kõrgel"
+                /* 
+                 * NB programm peab niikaua käima kuni kasutaja sisestus on suurem või võrdne kui 40.000
+                 */
+
+                // 2. "vibumees"
+                // tee muutuja "mehike" mille sees on täisarv 0
+                // tee muutuja "vastus" mille sees on tühi tekst
+                // kirjuta while tsükkel
+                // -> while tsükli tingimuseks kirjuta kontroll:
+                //      mis kontrollib kas muutujas "mehike" on täisarv 5 või vähem
+                //      või
+                //      muutujas "vastus" on tekst "jah"
+                // tsükli tegevuseks küsi kasutajalt "kas tahad mehikesse noole lasta?"
+                // tsükli tegevuseks pane muutujasse "vastus" väärtus kasutajalt käsurea pealt 
+                // tsükli tegevuseks kontrolli tingimuslausega kas vastus oli "jah"
+                // tsükli tegevuseks Kui tingimuslause vastus oli jah, siis:
+                // kuva kasutajale tekst "mitu noolt lased?"
+                // lisa muutujasse "mehike" kasutajalt saadud täisarv
+                // peale tsüklit kontrolli kas muutujas "mehike" olev arv on väiksem kui 5
+                // kui on väiksem kui 5 siis kuva sõnum "mehike elab :D"
+                // kõikidel muudel juhtudel kuva sõnum "tapsid mehikese ära :c"
+
+                /* For ülesanded*/
+
+                // 1. "prindi numbrid"
+                // tee muutuja "kuipalju" milles on täisarv 0 
+                // kuva kasutajale tekst millega küsid kasutajalt mitu numbrit ta tahab
+                // omista käsurealt saadud arv muutujasse "kuipalju"
+                // kirjuta for tsükkel, tsükli teise parameetrisse - kontrolli aseta i vastu muutuja "kuipalju"
+                // tsükli tegevusena kuva kasutajale välja i, aga liida sellele üks juurde et lugemine algaks arvust 1
+                // peale tsüklit kuva kasutajale sõnum "tsükkel lõppes"
+
+                // 2. "Ruut"
+                // tee muutuja "ruudukülg" milles on täisarv 0
+                // kuva kasutajale tekst millega küsid kasutajalt kui suurt ruutu ta tahab
+                // omista käsurealt saadud arv muutujasse "ruudukülg"
+                // kirjuta for tsükkel, tsükli tingimuse kontrolli pane i kontrollimisse muutuja "ruudukülg"
+                // tsükli sisse kirjuta sõne tüüpi muutuja, "seeRida", kuhu omistad tühja sõne.
+                // tsükli sees on omakorda teine for tsükkel, ka selle tingimuse kontrolli pane i kontrollimisse muutuja "ruudukülg"'
+                // nüüd sisemise tsükli sees, omista muutujale "seeRida" juurde liites sisse väärtus "HH" 
+                // kui sisemine tsükkel on lõpetanud, siis kuva kasutajale see rida välja
+                // kui ka esimene tsükkel on lõpetanud, siis kuva kasutajale tekst "tsüklid on lõpetanud"
+
+                int ruuduKülg = 0;
+            }
             Console.WriteLine("Kui suurt ruutu sa tahad?");
             ruuduKülg = int.Parse(Console.ReadLine());
             for (int i = 0; i < ruuduKülg; i++)
